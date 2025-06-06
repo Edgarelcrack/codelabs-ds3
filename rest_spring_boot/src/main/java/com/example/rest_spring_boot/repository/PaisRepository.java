329058ca1c3131cabcdf00501128434c04ca1319
Edgar Andres <143111073+Edgarelcrack@users.noreply.github.com>
2025-06-06 18:32:25 -0500

    Add files via upload

---
+++ rest_spring_boot/src/main/java/com/example/rest_spring_boot/repository/PaisRepository.java
@@ -0,0 +1,12 @@
+package com.example.rest_spring_boot.repository;
+
+import com.example.rest_spring_boot.model.Pais;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PaisRepository extends JpaRepository<Pais, Long> {
+    Optional<Pais> findByNombre(String nombre);
+}
